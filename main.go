f6786c0715b827c4bc912e7fa2272b2cc10032a2
Matt Gartman <mattgartman@gmail.com>
2019-01-07 07:26:40 -0500

    updating to chan

--- main.go
+++ main.go
@@ -11,7 +11,17 @@ import (
 func main() {
 	x, _ := url.Parse("http://localhost:8080")
 	test := abtester.ABTest{TestURL: *x, DurationSeconds: 12}
-	a := abtester.StartABtest(test)
+	go abtester.StartABtest(test)
+
+	for {
+		select {
+		case t := <-results:
+			ret = append(ret, t)
+			//fmt.Printf("Result: url: %v | id: %v | succeeded: %v", t.TestURL, t.WorkerID, t.Succeeded)
+		case <-chExit:
+			return
+		}
+	}
 
 	sort.Slice(a, func(i int, j int) bool {
 		if a[i].WorkerID == a[j].WorkerID {
